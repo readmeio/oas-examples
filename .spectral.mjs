c978df0d87a14a2b045c74b740981503be8f7771
rmaheshwari@enova.com <rmaheshwari@enova.com>
2023-08-08 11:49:40 -0500

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMzU2MA/spectral.js?branch_slug=main&token=9f80c3c2-2ca5-4cf8-bc39-b6c7dca8b9a6";
+export default { extends: ruleset };
